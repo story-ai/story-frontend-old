3884db3da957b9d3722a6257fda99d61bb7ae7c3
Christopher Little <hephistocles@gmail.com>
2018-03-06 17:02:25 +0000

    Add tracking code and change some wordings

--- src/core/epics/auth/register.ts
+++ src/core/epics/auth/register.ts
@@ -25,7 +25,7 @@ export const register: Epic<AllActions, StateType> = action$ =>
           message?: string;
         };
 
-        if (res.ok) {
+        if (data) {
           if (data.success)
             return [new RegisterSucceeded(), new LoginRequested()];
           return [new RegisterFailed(data.message)];
@@ -34,9 +34,11 @@ export const register: Epic<AllActions, StateType> = action$ =>
         console.error(res);
         return [new RegisterFailed("An unknown error occurred")];
       })
-      .catch(
-        e =>
-          console.error(e) ||
-          Observable.of(new RegisterFailed("An unknown error occurred"))
-      )
+      .catch(e => {
+        console.error(e);
+        if (e.response.body && !e.response.body.success) {
+          return [new RegisterFailed(e.response.body.message)];
+        }
+        return Observable.of(new RegisterFailed("An unknown error occurred"));
+      })
   );
